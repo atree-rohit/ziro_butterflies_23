bc32d6b6f2c1255318e3ab3ffe9751e1a706f89c
atree-rohit <rohit.george@atree.org>
2023-10-02 17:11:32 +0530

    pwa config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import { createApp } from 'vue'
-import store from './store'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import store from "./store"
+import "./style.css"
+import App from "./App.vue"
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then((registration) => {
+                console.log(
+                "Service Worker registered with scope:",
+                registration.scope
+                )
+            })
+            .catch((error) => {
+                console.error("Service Worker registration failed:", error)
+            })
+    })
+}
 
 const app = createApp(App)
 app.use(store)
-app.mount('#app')
+app.mount("#app")
