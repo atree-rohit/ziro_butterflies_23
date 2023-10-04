412852ce03a54b8a74ea8826515bf5ed0ce0e648
atree-rohit <rohit.george@atree.org>
2023-10-04 15:48:10 +0530

    images to idb

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
+import { openDb, savePhoto, getPhoto, getIDBlength } from '../utils/idb_photos.js';
+
+
 import family_details from '../assets/json/descriptions.json'
 import species from '../assets/json/species.json'
 import species_details from '../assets/json/species_descriptions.json'
@@ -9,6 +14,7 @@ import users from '../assets/json/users.json'
 export default createStore({
     state: {
         json_data: {},
+        photos:{}
     },
     getters: {
         species_array(state){
@@ -40,6 +46,10 @@ export default createStore({
     mutations: {
         SET_JSON_DATA(state, payload) {
             state.json_data[payload.field] = payload.data
+        },
+        SET_PHOTO(state, payload) {
+            const {family, species, value} = payload
+            state.photos[`${family}_${species}`] = value
         }
     },
     actions: {
@@ -62,13 +72,40 @@ export default createStore({
                     data: users
                 }
             ]
-
+            
             json_data.forEach((item) => {
                 dispatch('setJsonData', item)
             })
+            if(getIDBlength() === 0){
+                const families = ["Papilionidae", "Nymphalidae", "Lycaenidae", "Pieridae", "Riodinidae", "Hesperiidae"]
+                families.forEach(async (family) => await dispatch('getPhotos', family))
+            } else {
+                dispatch('storePhotos')
+            }
+
         },
         setJsonData({ commit }, payload) {
             commit('SET_JSON_DATA', payload)
+        },
+        async getPhotos({ commit }, family) {
+            const url = `/assets/photos/${family}_photos.json`
+            const response = await axios.get(url)
+            if(response){
+                Object.entries(response.data).forEach(async ([key, value]) => await savePhoto(family, key, value))
+            }
+        },
+        storePhotos({getters, dispatch, commit}){
+            getters.species_array.forEach(async (s) => {
+                const payload = {
+                    family: s.family,
+                    species: s.scientific_name,
+                    value: await getPhoto(s.family, s.scientific_name)
+                }
+                commit('SET_PHOTO', payload)
+
+            })
+
         }
+
     }
 })
---
+++ src/utils/idb_photos.js
@@ -0,0 +1,46 @@
+// Import the idb library if you haven't already
+import { openDB } from "idb";
+
+const DB_NAME = "photoDB"; // Change to your desired database name
+const STORE_NAME = "photos"; // Change to your desired store name
+
+// Function to open the IndexedDB database
+async function openDb() {
+    return openDB(DB_NAME, 1, {
+        upgrade(db) {
+            db.createObjectStore(STORE_NAME);
+        },
+    });
+}
+
+// Function to save a photo in the database
+async function savePhoto(family, species, base64ImageData) {
+    const db = await openDb();
+    const tx = db.transaction(STORE_NAME, "readwrite");
+    const store = tx.objectStore(STORE_NAME);
+    const key = `${family}_${species}`;
+
+    await store.put(base64ImageData, key);
+    await tx.done;
+}
+
+// Function to get a photo from the database
+async function getPhoto(family, species) {
+    const db = await openDb();
+    const tx = db.transaction(STORE_NAME, "readonly");
+    const store = tx.objectStore(STORE_NAME);
+    const key = `${family}_${species}`;
+
+    return store.get(key);
+}
+
+async function getIDBlength() {
+    // Implement a function to check if the IndexedDB database is empty
+    // You can use openDb and count records in the database
+    const db = await openDb();
+    const tx = db.transaction(STORE_NAME, 'readonly');
+    const store = tx.objectStore(STORE_NAME);
+    return await store.count();
+}
+
+export { openDb, savePhoto, getPhoto, getIDBlength };
