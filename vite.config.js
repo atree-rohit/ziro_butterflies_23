36d5bd2f34a24dc4b9e738e785aa163a209351ff
atree-rohit <rohit.george@atree.org>
2023-10-04 16:02:49 +0530

    cache json?

--- vite.config.js
+++ vite.config.js
@@ -78,7 +78,7 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 30000000,
         globPatterns: [
           "**/*.{js,css,html,png,jpg,jpeg,gif,svg}",
-          "assets/photos/**/*.*",
+          "assets/photos/*.json",
         ],
       },
     }),
