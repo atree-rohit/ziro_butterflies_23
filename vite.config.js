0d426c631fcca41a89f93eb9f21c5184458eb1c8
atree-rohit <rohit.george@atree.org>
2023-10-08 18:56:59 +0530

    nrb

--- vite.config.js
+++ vite.config.js
@@ -78,7 +78,7 @@ export default defineConfig({
         maximumFileSizeToCacheInBytes: 30000000,
         globPatterns: [
           "**/*.{js,css,html,png,jpg,jpeg,gif,svg}",
-          "assets/photos/*.json",
+          "/ziro_butterflies_23/assets/photos/*.json",
         ],
       },
     }),
